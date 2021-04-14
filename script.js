0f23f94d1697a3d0ffb4966a8350c270f0250c3a
João Lucas <joaolucasfreitaslul@gmail.com>
2021-04-14 16:50:32 -0400

    Added the style.css and the script.js with the functions

---
+++ script.js
@@ -0,0 +1,17 @@
+const elements = document.querySelectorAll('.btn')
+
+
+elements.forEach(element => {
+    element.addEventListener('click', () => {
+        let command = element.dataset['element']
+
+        console.log(command)
+        if(command == 'createLink' || command == 'insertImage'){
+            let url = prompt('Enter the link here:', 'https://')
+            document.execCommand(command, false, url)
+
+        }  else {
+            document.execCommand(command, false, null)
+        }
+    })
+}) 
